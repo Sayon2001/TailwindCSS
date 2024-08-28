6aa2ad4feb5bdf640b558c769eb9838bf7b77b18
Sayon2001 <sayon.shrestha2001@gmail.com>
2024-08-28 23:10:19 +0545

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        domine : ['Domine', 'Sans-serif']
+      }
+    },
   },
   plugins: [],
 }
